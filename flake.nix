f4b176a853c59c16956684bc6a3dd77ad178cff6
NACAMURA Mitsuhiro <m15@m15a.dev>
2024-03-22 21:22:03 +0900

    ci: check Lua 5.1-5.4 and LuaJIT
    It uses frontier pattern, which is not found in Lua 5.1 manual.
    So I've thought it isn't available in Lua 5.1, but it IS actually.

--- flake.nix
+++ flake.nix
@@ -19,23 +19,58 @@
             inputs.fnldoc.overlays.default
           ];
         };
+
+        mkCICheckShell = { gnumake, fennel, faith, fnldoc }:
+          pkgs.mkShell {
+            buildInputs = [ gnumake fennel fennel.lua faith fnldoc ];
+          };
+
+        builder = { fennelVariant, luaVariant }:
+          let
+            fennelName =
+              if fennelVariant == "stable"
+              then "fennel-${luaVariant}"
+              else "fennel-${fennelVariant}-${luaVariant}";
+          in {
+            name = "ci-check-shell-${fennelName}";
+            value = pkgs.callPackage mkCICheckShell {
+              fennel = pkgs.${fennelName};
+              faith = pkgs.faith-unstable;
+            };
+          };
+
+        ci-check-shells = pkgs.lib.listToAttrs
+          (map builder (pkgs.lib.attrsets.cartesianProductOfSets {
+            fennelVariant = [ "stable" "unstable" ];
+            luaVariant = [ "lua5_1" "lua5_2" "lua5_3" "lua5_4" "luajit" ];
+          }));
       in
       {
-        devShells.default =
-          let
-            fennel = pkgs.fennel-unstable-luajit;
-          in
-          pkgs.mkShell {
-            buildInputs = [
-              pkgs.gnumake
-              fennel
-              fennel.lua
-              pkgs.fnlfmt-unstable
-              pkgs.fennel-ls-unstable
-              pkgs.fnldoc
-            ] ++ (with fennel.lua.pkgs; [
-              readline
-            ]);
+        devShells = {
+          inherit (ci-check-shells)
+          ci-check-shell-fennel-unstable-lua5_1
+          ci-check-shell-fennel-unstable-lua5_2
+          ci-check-shell-fennel-unstable-lua5_3
+          ci-check-shell-fennel-unstable-lua5_4
+          ci-check-shell-fennel-unstable-luajit;
+
+          default =
+            let
+              fennel = pkgs.fennel-unstable-luajit;
+            in
+            pkgs.mkShell {
+              buildInputs = [
+                pkgs.gnumake
+                fennel
+                fennel.lua
+                pkgs.fnlfmt-unstable
+                pkgs.fennel-ls-unstable
+                pkgs.faith-unstable
+                pkgs.fnldoc
+              ] ++ (with fennel.lua.pkgs; [
+                readline
+              ]);
+            };
           };
       });
 }
